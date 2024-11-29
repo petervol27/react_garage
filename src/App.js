0de628a9f5b8bf688b1d2101fc501eef918ea332
petervol27 <petervol35@gmail.com>
2024-11-29 10:18:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [cars, setCars] = useState([
+    { name: 'Honda', number: '123-45-678' },
+    { name: 'Toyota', number: '321-54-786' },
+    { name: 'Tesla', number: '555-21-444' },
+  ]);
+  const addCar = () => {
+    const newCar = { name: name, number: number };
+    setCars([...cars, newCar]);
+    alert(name + ' Added');
+    setName('');
+    setNumber('');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container text-center mt-3">
+        <div className="mb-3">
+          <label htmlFor="name">Name:</label>
+          <input
+            name="name"
+            className="ms-3"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+          <label htmlFor="number" className="ms-3">
+            Number:
+          </label>
+          <input
+            name="number"
+            className="ms-3"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+          ></input>
+          <button className="btn btn-primary ms-3" onClick={addCar}>
+            Add Car
+          </button>
+        </div>
+        {cars.map((car, index) => (
+          <div className="card mb-3 w-25 mx-auto bg-light" key={index}>
+            <div className="card-body">
+              <h5 className="card-title">{car.name}</h5>
+              <p className="card-text">Car number: {car.number}</p>
+              <button
+                href="#"
+                className="btn btn-primary"
+                onClick={() => alert(car.name)}
+              >
+                Check it out
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
